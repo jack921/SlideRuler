59eff16cc17ebc9105da258e4026bea61ebe99ed
jack921 <355596943@qq.com>
2017-05-05 01:55:38 +0800

    init

--- app/src/main/java/com/jack/slideruler/SlideRuler.java
+++ app/src/main/java/com/jack/slideruler/SlideRuler.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.Display;
@@ -17,42 +18,45 @@ import android.view.WindowManager;
 public class SlideRuler extends View{
 
     //最小值
-    private float minValue=0;
+    private int minValue=0;
     //最大值
-    private float maxValue=199000;
+    private int maxValue=199000;
     //当前值
-    private float currentValue=10000;
+    private int currentValue=10000;
     //最小单位值
-    private float minUnitValue=1000;
+    private int minUnitValue=1000;
+    //最小当前值
+    private int minCurrentValue=1000;
     //字体大小
     private int textSize;
     //字体颜色
     private int textColor;
     //线颜色
-    private int dividerColor;
-    //最小当前值
-    private float minCurrentValue;
+    private int dividerColor=Color.parseColor("#000000");
     //指示线颜色
-    private int indicatrixColor;
+    private int indicatrixColor=Color.parseColor("#000000");
+    //画线的画笔
+    private Paint linePaint;
 
-    private int WrapcontentWidth;
-    private int WrapcontentHight;
+    private int marginWidth=0;
+    private int wrapcontentWidth;
+    private int wrapcontentHight;
 
     private Display display =null;
 
-    public void setMinValue(float minValue) {
+    public void setMinValue(int minValue) {
         this.minValue = minValue;
     }
 
-    public void setMaxValue(float maxValue) {
+    public void setMaxValue(int maxValue) {
         this.maxValue = maxValue;
     }
 
-    public void setCurrentValue(float currentValue) {
+    public void setCurrentValue(int currentValue) {
         this.currentValue = currentValue;
     }
 
-    public void setMinUnitValue(float minUnitValue) {
+    public void setMinUnitValue(int minUnitValue) {
         this.minUnitValue = minUnitValue;
     }
 
@@ -72,7 +76,7 @@ public class SlideRuler extends View{
         this.indicatrixColor = indicatrixColor;
     }
 
-    public void setMinCurrentValue(float minCurrentValue) {
+    public void setMinCurrentValue(int minCurrentValue) {
         this.minCurrentValue = minCurrentValue;
     }
 
@@ -87,7 +91,7 @@ public class SlideRuler extends View{
     public SlideRuler(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context,attrs,defStyleAttr);
         display=((WindowManager)getContext().getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
-        WrapcontentWidth=display.getWidth();
+        wrapcontentWidth=display.getWidth();
 
         TypedArray typedArray=context.getTheme().obtainStyledAttributes(attrs,R.styleable.slideruler,defStyleAttr,0);
         int numCount=typedArray.getIndexCount();
@@ -95,19 +99,19 @@ public class SlideRuler extends View{
             int attr=typedArray.getIndex(i);
             switch(attr){
                 case R.styleable.slideruler_minValue:
-                    minValue=typedArray.getFloat(i,0);
+                    minValue=typedArray.getInteger(i,0);
                     break;
                 case R.styleable.slideruler_maxValue:
-                    maxValue=typedArray.getFloat(i,199000);
+                    maxValue=typedArray.getInteger(i,199000);
                     break;
                 case R.styleable.slideruler_currentValue:
-                    currentValue=typedArray.getFloat(i,10000);
+                    currentValue=typedArray.getInteger(i,10000);
                     break;
                 case R.styleable.slideruler_minUnitValue:
-                    minCurrentValue=typedArray.getFloat(i,1000);
+                    minCurrentValue=typedArray.getInteger(i,1000);
                     break;
                 case R.styleable.slideruler_minCurrentValue:
-                    minCurrentValue=typedArray.getFloat(i,1000);
+                    minCurrentValue=typedArray.getInteger(i,1000);
                     break;
                 case R.styleable.slideruler_textSize:
                     textSize=typedArray.getDimensionPixelSize(attr,(int) TypedValue.applyDimension(
@@ -124,6 +128,11 @@ public class SlideRuler extends View{
                     break;
             }
         }
+
+        linePaint=new Paint();
+        linePaint.setAntiAlias(true);
+        linePaint.setStyle(Paint.Style.STROKE);
+
     }
 
     @Override
@@ -138,19 +147,46 @@ public class SlideRuler extends View{
         if(widthModel==MeasureSpec.EXACTLY){
             width=widthSize;
         }else{
-            width=WrapcontentWidth;
+            width=wrapcontentWidth;
         }
         if(heightModel==MeasureSpec.EXACTLY){
             height=heightSize;
         }else{
-            height=(int)(getPaddingBottom()+getPaddingTop()+(WrapcontentHight/4));
+            height=(int)(getPaddingBottom()+getPaddingTop()+(wrapcontentHight/4));
         }
         setMeasuredDimension(width,height);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+        marginWidth=getWidth()/30;
+        drawBaseLine(canvas);
+        drawBaseView(canvas);
     }
+
+    //画最基础的两条线
+    public void drawBaseLine(Canvas canvas){
+        linePaint.setColor(indicatrixColor);
+        canvas.drawLine(getWidth()/2,0,getWidth()/2,getHeight(),linePaint);
+        linePaint.setColor(dividerColor);
+        canvas.drawLine(0,getHeight(),getWidth(),getHeight(),linePaint);
+    }
+
+    //画初始的界面
+    public void drawBaseView(Canvas canvas){
+        int left= currentValue/minUnitValue;
+        for(int i=1;i<left+1;i++){
+            int xValue=getWidth()/2-(marginWidth*i);
+            if(i/10==0){
+                canvas.drawLine(xValue,getHeight(),xValue,getHeight()-25,linePaint);
+            }else{
+                canvas.drawLine(xValue,getHeight(),xValue,getHeight()-40,linePaint);
+            }
+        }
+
+
+    }
+
+
 
 }
